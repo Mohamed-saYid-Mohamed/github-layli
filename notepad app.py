8a2ee85bdfe6ed7fedaaab4cc243c687a3335a93
Mohamed SaYid Mohamed <mohammeddeeqsi90@gmail.com>
2026-07-22 10:56:25 +0300

    81 - waxaan ku daray fariin cusub Python

--- notepad app.py
+++ notepad app.py
@@ -1,45 +1,79 @@
 print("Ku soo dhawoow Python project")
+
 magac = "Mohamed"
 print(magac)
+
 da = 24
 print(da)
+
 xirfad = "Programming"
 print(xirfad)
+
+
 def salaam():
     print("Salaan Mohamed")
 
+
 salaam()
+
+
 liis = ["Python", "HTML", "CSS"]
 print(liis)
+
 for item in liis:
     print(item)
 
-    if da >= 18:
+
+if da >= 18:
     print("Qof weyn")
-    user = {
+
+
+user = {
     "magac": "Mohamed",
     "xirfad": "Python"
 }
 
 print(user)
+
+
 x = 10
 y = 20
 print(x + y)
+
+
 print("Python waa luuqad aan jeclahay")
+
+
 magacyo = ["Ali", "Mohamed", "Ahmed"]
-print(magacyo)
+
 for magac in magacyo:
     print(magac)
-    def isku_dar(a,b):
-    return a+b
-natiijo = isku_dar(5,3)
+
+
+def isku_dar(a, b):
+    return a + b
+
+
+natiijo = isku_dar(5, 3)
 print(natiijo)
+
+
 class Arday:
     pass
+
+
 print("Waxaan baranayaa Python maalin kasta")
+
+
 xog = "Git practice"
 print(xog)
-numbers = [1,2,3,4]
+
+
+numbers = [1, 2, 3, 4]
 print(numbers)
+
+
 print("Mashruuca Python wuu socdaa")
+
 print("Python project qaybtiisa koowaad waa dhammaatay")
+print("Waxaan sii wadayaa barashada Python")
