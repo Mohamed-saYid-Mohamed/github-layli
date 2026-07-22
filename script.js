8cdbddcc2d69d93d3e80c5a09125030f6688ca8f
Mohamed SaYid Mohamed <mohammeddeeqsi90@gmail.com>
2026-07-22 10:29:09 +0300

    55 - waxaan ku daray if statement

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function salaam(){
     console.log("Salaan Mohamed");
 }
 salaam();
+let xirfado = ["HTML", "CSS", "JavaScript"];
+console.log(xirfado);
+let da = 24;
+
+if(da >= 18){
+    console.log("Qof weyn");
+}
